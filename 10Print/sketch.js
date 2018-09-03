fee1c8738996b31f0b7f99d4a0f5430eb5983589
yllfejziu <38266916+yllfejziu@users.noreply.github.com>
2018-09-03 09:44:46 +0200

    Update sketch.js

--- 10Print/sketch.js
+++ 10Print/sketch.js
@@ -1,40 +1,48 @@
-let space = 20;
-let r = 30;
-let g = 255;
-let b = 70;
+let space = 30;
+let r;
+let g;
+let b;
 let x = 0;
 let y = 0;
 
 function setup(){
 	createCanvas(windowWidth, windowHeight);
 	background(0);
+	r = random(255);
+	g = random(255);
+	b = random(255);
 }
 
 function draw(){
 	stroke(r,g,b);
-	strokeWeight(4);
+	strokeWeight(7);
 	fill(255);
 	
-	if(random(1) < 0.5)
-	   line(x, y, x + space, y + space);
-	else
-	   line(x + space, y, x, y + space);
+	if(random(1) < 0.5){
+		line(x, y, x + space, y + space);
+	}else{
+		line(x + space, y, x, y + space);
+	}
 
 	x+= space;
 
 	if(y >= windowHeight / 2 && x >= windowWidth){
 		x = 0;
 		y += space;
-		r -= 30;
-		b += 30;
-		g += 20;
+		r -= random(30);
+		b += random(30);
+		g += random(30);
 		console.log(true);
 	}else if(x >= windowWidth){
 		x = 0;
 		y += space;
-		r += 20;
-		g -= 20;
-		b -= 20;
+		r += random(30);
+		g -= random(30);
+		b -= random(30);
 	}
-
+	if(y >= height){
+		background(0);
+		x=0;
+		y=0;
+	}
 }
